65c26e1760ab8695443d87cf75690d964bec4cb4
Pathan Zaid <zaidpathan339@gmail.com>
2020-04-09 13:23:41 +0530

    Updates

---
+++ precache-manifest.efd2ff66525d99ea5e45e8e91254f7a3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5576eb26df77f5159c8cc23e69a34a0",
+    "url": "/react-task3/index.html"
+  },
+  {
+    "revision": "43c77b5560d72fd15a0a",
+    "url": "/react-task3/static/css/main.1f20b9f7.chunk.css"
+  },
+  {
+    "revision": "e20905650c6ff80860dc",
+    "url": "/react-task3/static/js/2.9edfc25e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-task3/static/js/2.9edfc25e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43c77b5560d72fd15a0a",
+    "url": "/react-task3/static/js/main.e8d70b83.chunk.js"
+  },
+  {
+    "revision": "f3923d7fbcb092914b3c",
+    "url": "/react-task3/static/js/runtime-main.53b4ef19.js"
+  },
+  {
+    "revision": "15f6ce85d9a8e10289dea5a2b12e453a",
+    "url": "/react-task3/static/media/calender-icon.15f6ce85.png"
+  },
+  {
+    "revision": "fd0c9d30c4adf9d46ba04fa6abeb5449",
+    "url": "/react-task3/static/media/card-background.fd0c9d30.jpg"
+  },
+  {
+    "revision": "a1167211bffbc81f616db1ef850aee2d",
+    "url": "/react-task3/static/media/empty-list-icon.a1167211.png"
+  },
+  {
+    "revision": "e4b6323e19e57f6761b1ce47b7d329e0",
+    "url": "/react-task3/static/media/loader.e4b6323e.gif"
+  },
+  {
+    "revision": "bb6dc57cba6bcd6d5ec5980bc71b153b",
+    "url": "/react-task3/static/media/logout-icon.bb6dc57c.png"
+  }
+]);
